797f9a85700154ab11173070e684764816ec3697
Augstoo22 <victoraugustodocarmo32@gmail.com>
2024-09-13 20:48:57 -0300

    feat: tela de login

---
+++ mobile/src/app/index.tsx
@@ -0,0 +1,22 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from '@react-navigation/stack';
+import { HomeScreen } from "../components/home";
+import { DetailsScreen } from "../components/details";
+
+type RootStackParamList = {
+  Home: undefined;
+  Details: {name: string; email: string};
+};
+
+const Stack = createStackNavigator<RootStackParamList>();
+
+export default function Index() {
+  return (
+    <NavigationContainer independent={true}>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen}   options={{ headerShown: false }} />
+        <Stack.Screen  name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
