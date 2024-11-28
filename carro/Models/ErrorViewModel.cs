fd702bc92ed7c86f5dd9251a98bec66138731bd4
Victor Augusto do Carmo <victoraugustodocarmo32@gmail.com>
2024-11-27 21:35:50 -0300

    feat: estrutura inicial da api carros

---
+++ carro/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace carro.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ carro/Program.cs
@@ -0,0 +1,57 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.UseUrls("http://0.0.0.0:50");
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// Adicionando Swagger ao serviço
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Configurando CORS para permitir acesso de http://localhost:8080
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSwaggerUI", policy =>
+    {
+        policy.WithOrigins("http://localhost:8080")
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials(); // Permite credenciais se necessário
+    });
+});
+
+var app = builder.Build();
+
+// Aplicando o middleware de CORS antes dos outros middlewares
+app.UseCors("AllowSwaggerUI");
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+else
+{
+    // Configurando Swagger apenas para o ambiente de desenvolvimento
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API Carro");
+        c.RoutePrefix = "swagger";
+    });
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
