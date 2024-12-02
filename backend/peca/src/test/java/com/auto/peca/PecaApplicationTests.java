df1fd84ddf1c9209a0ab2d9875e6c03f7550379f
Victor Augusto do Carmo <victoraugustodocarmo32@gmail.com>
2024-12-02 16:54:20 -0300

    feat: estrutura do backend em Java

---
+++ backend/peca/src/test/java/com/auto/peca/PecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auto.peca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
