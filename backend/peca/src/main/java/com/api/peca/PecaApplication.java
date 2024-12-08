072c5bf8258b37c9d2a2d5210e7c914debe289ca
Victor Carmo <victoraugustodocarmo32@gmail.com>
2024-12-08 15:21:30 -0300

    feat: criado api de pecas com mvc

--- backend/peca/src/main/java/com/api/peca/PecaApplication.java
+++ backend/peca/src/main/java/com/api/peca/PecaApplication.java
@@ -1,4 +1,4 @@
-package com.auto.peca;
+package com.api.peca;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PecaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PecaApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(PecaApplication.class, args);
+    }
 }
--- backend/peca/src/test/java/com/api/peca/PecaApplicationTests.java
+++ backend/peca/src/test/java/com/api/peca/PecaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.auto.peca;
+package com.api.peca;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
